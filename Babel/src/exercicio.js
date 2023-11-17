3ad988068fc63560cdd1536569451827617898f6
spotivyie <eduardac.brandao@hotmail.com>
2023-11-17 15:31:06 -0400

    add

---
+++ Babel/src/exercicio.js
@@ -0,0 +1,23 @@
+const nomeDoAluno = nome => {
+    let notaDoAluno = ''
+
+    for (let i=0; i< notas.length; i++){
+        if (nome === notas[i].nome){
+            notaDoAluno = nota
+        }
+    }
+}
+
+const notas = [
+    {nome: 'julia', nota: 5},
+    {nome: 'juliana', nota: 6},
+    {nome: 'paula', nota: 10},
+    {nome: 'joão', nota: 3},
+    {nome: 'carlos', nota: 8}
+]
+
+const filtraNotas = notas.filter(function(item){
+    return item.nota >= 6
+})
+
+console.log(filtraNotas)
