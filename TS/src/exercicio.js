a34061c0da78d7506aa4a4d91662d133ade5b11d
spotivyie <eduardac.brandao@hotmail.com>
2023-11-18 23:54:28 -0400

    add

---
+++ TS/src/exercicio.js
@@ -0,0 +1,9 @@
+"use strict";
+function multiplicacao(base, altura) {
+    return base * altura;
+}
+const saudacao = (nome) => {
+    return `Olá ${nome}`;
+};
+console.log(multiplicacao(20, 10));
+console.log(saudacao("Ana"));
---
+++ TS/src/exercicio.ts
@@ -0,0 +1,10 @@
+function multiplicacao (base: number, altura:number): number {
+    return base * altura;
+}
+
+const saudacao = (nome: string): string =>{
+    return `Olá ${nome}`;
+}
+
+console.log(multiplicacao(20, 10));
+console.log(saudacao("Ana"));
